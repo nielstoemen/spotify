dfd5856bddceba757e3d3bcb45ad069c237732be
nielstoemen <nielstoemen@gmail.com>
2023-05-25 11:46:07 +0200

    Add project files.

---
+++ spotify1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace spotify1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
