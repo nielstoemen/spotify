8088fb40579a6e6fd35e774cc1f94ff6064a76af
robinmulders <1190447@student.roc-nijmegen.nl>
2023-06-08 11:35:46 +0200

    person aangemaakt

--- spotify1/Program.cs
+++ spotify1/Program.cs
@@ -5,6 +5,18 @@ namespace spotify1
     {
         static void Main(string[] args)
         {
+            Person user = new Person();
+
+
+            Console.WriteLine("Enter username:");
+            user.Username = Console.ReadLine();
+            Console.WriteLine("Username is: " + user.Username);
+            user.accountcode = 12345;
+
+          
+
+          
+
         }
     }
 }
